611351fd1da0d5005ccdddf035c14a263a08480d
Daniel Vena <deaconvena+github@gmail.com>
2022-07-20 19:34:58 -0400

    remove 4th addthis option

--- src/functionality/addthis.js
+++ src/functionality/addthis.js
@@ -75,7 +75,6 @@ import { addAfterPageFrame } from 'Utils/playerTools';
 				<a class="addthis_button_preferred_1"></a>
 				<a class="addthis_button_preferred_2"></a>
 				<a class="addthis_button_preferred_3"></a>
-				<a class="addthis_button_preferred_4"></a>
 				<a class="addthis_button_compact"></a>
 			`;
 
